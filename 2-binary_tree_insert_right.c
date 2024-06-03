2de1c1fdb504e56d7688a2bf03905f0a0dddd162
Mbaruku2003 <leonardjembe21@gmail.com>
2024-06-03 15:00:13 -0700

    we first check wheather a leaf is NULL whichmeans it is an invalid pointer, we will then return 0the function then checks for the child nodes to see wheather to the right or left is NULL

--- 2-binary_tree_insert_right.c
+++ 2-binary_tree_insert_right.c
@@ -3,8 +3,8 @@
 #include <stdio.h>
 /**
  * binary_tree_insert_right - inserts a node as the right-child of another node
- * parent:  a pointer to the node to insert the right-child in
- * value: the value to store in the new node
+ * @parent:  a pointer to the node to insert the right-child in
+ * @value: the value to store in the new node
  * Return: a pointer to created node or NULL
  */
 binary_tree_t *binary_tree_insert_right(binary_tree_t *parent, int value)
---
+++ 4-binary_tree_is_leaf.c
@@ -0,0 +1,19 @@
+#include "binary_trees.h"
+#include <stdlib.h>
+/**
+ * binary_tree_is_leaf - checks if a node is a leaf
+ * @node: a pointer to the node to check
+ * Return: 1 if node is a leaf else 0
+ */
+int binary_tree_is_leaf(const binary_tree_t *node)
+{
+	/** Check if  node is NULL **/
+	if (node == NULL)
+	{
+		return (0);
+	}
+
+	/** check if both the left node and right node are null. **/
+	return node->left == NULL && node->right == NULL;
+}
+
