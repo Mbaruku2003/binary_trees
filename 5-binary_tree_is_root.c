62464e436cb280a1ec706952029aff26e670ab86
Mbaruku2003 <leonardjembe21@gmail.com>
2024-06-03 23:55:20 -0700

    5-binary_tree_is_root.c

--- 5-binary_tree_is_root.c
+++ 5-binary_tree_is_root.c
@@ -3,7 +3,7 @@
 /**
  * binary_tree_is_root - checks if a given node is a root
  * @node: a pointer to the node to check
- * Return 1 if node is a root else 0
+ * Return: 1 if node is a root else 0
  */
 int binary_tree_is_root(const binary_tree_t *node)
 {
@@ -13,5 +13,5 @@ int binary_tree_is_root(const binary_tree_t *node)
 	}
 
 	/** roots parent pointer is always NULL **/
-	return node->parent == NULL;
+	return (node->parent == NULL);
 }
