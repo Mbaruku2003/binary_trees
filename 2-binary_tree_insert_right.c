8d3583fcbf38c7fdcc9408062bc5175ed334190b
Mbaruku2003 <leonardjembe21@gmail.com>
2024-06-04 00:39:02 -0700

    we tested wheather parent and new node are null and returned NULL, we then added our existing node to the new node of parent and updatedto point to the new node as long as parent has a right node

--- 2-binary_tree_insert_right.c
+++ 2-binary_tree_insert_right.c
@@ -9,26 +9,22 @@
  */
 binary_tree_t *binary_tree_insert_right(binary_tree_t *parent, int value)
 {
-	/** check if memory allocation has failed. **/
-	/** check if parent node exists. **/
-	if (!parent || !binary_tree_node(NULL, value))
+	binary_tree_t *new_node;
+
+	if (parent == NULL)
 	{
 		return (NULL);
 	}
-
-	/** create a new node from the existing binary tree structure.**/
-	/** to do this we use binary_tree_node(parent, value.**/
-	binary_tree_t *new_node = binary_tree_node(parent, value);
-
-	/***Check if parent already has a right child.**/
+	new_node = binary_tree_node(parent, value);
+	if (new_node == NULL)
+	{
+		return (NULL);
+	}
 	if (parent->right)
 	{
-		/**lect child parent pointer is set t point to new node.**/
-		parent->right->parent = new_node;
-		/** the new node left child is set to the existing node.**/
 		new_node->right = parent->right;
+		parent->right->parent = new_node;
 	}
-	/**parent node's left child pointer is updated to point to the new**/
 	parent->right = new_node;
 	return (new_node);
 }
