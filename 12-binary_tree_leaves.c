655d95f16512da1213d5db1869d337708b3407db
Mbaruku2003 <leonardjembe21@gmail.com>
2024-06-04 07:28:16 -0700

    we first test if the tree exists then wheather the right and left trees exist , we then recursively count left subtrees and right subtrees and return their result

--- 12-binary_tree_leaves.c
+++ 12-binary_tree_leaves.c
@@ -3,7 +3,7 @@
 #include <stddef.h>
 /**
  * binary_tree_leaves - counts the leaves in a binary tree
- * @tree - the pointer to the root node of the tree
+ * @tree: the pointer to the root node of the tree
  * Return: 0 if the tree is NULL
  */
 size_t binary_tree_leaves(const binary_tree_t *tree)
