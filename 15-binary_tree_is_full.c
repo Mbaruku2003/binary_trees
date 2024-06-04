1c394cdaee295d96e907fec81275c7bf51949359
Mbaruku2003 <leonardjembe21@gmail.com>
2024-06-04 08:59:42 -0700

    to check if binary tree is full, check if both right and left tree exist and if tree is fully recursive

---
+++ 15-binary_tree_is_full.c
@@ -0,0 +1,22 @@
+#include "binary_trees.h"
+#include <stddef.h>
+#include <stdlib.h>
+/**
+ * binary_tree_is_full - checks if a binary tree is full
+ * @tree: the root node
+ * Return: 0 if tree is NULL
+ */
+int binary_tree_is_full(const binary_tree_t *tree)
+{
+	if (tree == NULL)
+	{
+		return (0);
+	}
+	/** chcks if both right child and left child exist **/
+	if (!tree->left || !tree->right)
+	{
+		return (0);
+	}
+	/** checks if subtree is fully recursive **/
+	return (binary_tree_is_full(tree->left) == binary_tree_is_full(tree->right));
+}
