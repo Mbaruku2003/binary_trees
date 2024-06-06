28e4dedc6d9e6475902fac51671d593ec1c631e8
Mbaruku2003 <leonardjembe21@gmail.com>
2024-06-06 12:13:03 -0700

    103-binary_tree_rotate_left.c

--- 103-binary_tree_rotate_left.c
+++ 103-binary_tree_rotate_left.c
@@ -16,7 +16,23 @@ binary_tree_t *binary_tree_rotate_left(binary_tree_t *tree)
 	new_root = tree->right;
 	/**Update left child of the new root**/
 	tree->right = new_root->left;
-	/**Update parent of the new root (becomes the current root)**/
+	if (new_root->left != NULL)
+	{
+		new_root->left->parent = tree;
+	}
+	new_root->parent = tree->parent;
+	if (tree->parent != NULL)
+	{
+		if (tree->parent->left == tree)
+		{
+			tree->parent->left = new_root;
+		}
+		else
+		{
+			tree->parent->right = new_root;
+		}
+	}
 	new_root->left = tree;
+	tree->parent = new_root;
 	return (new_root);
 }
